d9811e6730e82de8efac76cc6247bd72bc6dfa9c
Yug Divyang Patel <yugpatel196@gmail.com>
2026-04-01 13:28:55 +0530

    trying to get full

--- q1/q1.s
+++ q1/q1.s
@@ -23,10 +23,12 @@ null_done:
  addi sp,sp, 16
  ret
 
+
 insert:
   bne a0,x0,recursion
   mv a0,a1
   j make_node
+
 recursion:
    addi sp, sp, -32
    sd ra, 24(sp)
@@ -36,7 +38,8 @@ recursion:
    mv s1, a1
    lw t0,0(s0)
 
-   bge s1,t0,right
+   blt s1,t0,left
+   j right
 
 left:
  ld a0,8(s0)
@@ -59,37 +62,48 @@ insertion_end:
   addi sp,sp, 32
   ret
 
+
 get:
-  bne a0, x0, check
-  ret
+  beq a0, x0, return_null
+  j check
+
 check: 
   lw t0, 0(a0)            
-  ld t1, 8(a0)            
   beq t0, a1, found       
-  bge a1, t0, get_right   
+  bgt a1, t0, get_right   
+
 get_left:                 
-  mv a0, t1               
+  ld a0, 8(a0)               
   j get  
+
 get_right:
   ld a0, 16(a0)           
   j get
+
 found:
   ret
 
+return_null:
+  mv a0, x0
+  ret
+
+
 getAtMost:
   li t2, -1
-  bne a1, x0, loop
-  j end
+  beq a1, x0, end
+
 loop:
   lw t0, 0(a1)
-  bgt t0, a0, getAtMost_left
+  bgt t0, a0, go_left
   mv t2, t0
   ld a1, 16(a1)
   bne a1, x0, loop
   j end
-getAtMost_left:
+
+go_left:
   ld a1, 8(a1)
   bne a1, x0, loop
+
 end:
   mv a0, t2
   ret
